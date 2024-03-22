72a44c970a16116cde815a6ae59ef90a895ca2c0
Muhammad Owais Qureshi <owaisqureshi34667@gmail.com>
2024-03-23 02:17:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        message: "Enter your first value",
+        type: "number",
+        name: "Firstvalue",
+    },
+    {
+        message: "Enter your seconde value",
+        type: "number",
+        name: "Secondevalue",
+    },
+    {
+        mesassge: "Select opprator",
+        type: "list",
+        name: "Oprator",
+        choices: ['Addition', 'Subtraction', 'Multipiction', 'Divison']
+    }]);
+if (answer.Oprator === "Addition") {
+    console.log(answer.Firstvalue + answer.Secondevalue);
+}
+else if (answer.Oprator === "Subtraction") {
+    console.log(answer.Firstvalue - answer.Secondevalue);
+}
+else if (answer.Oprator === "Multipiction") {
+    console.log(answer.Firstvalue * answer.Secondevalue);
+}
+else if (answer.Oprator === "Divison") {
+    console.log(answer.Firstvalue / answer.Secondevalue);
+}
